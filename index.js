5e0dbd19749338aea7768e46e4e0477f12967b02
Martin Pavlik <martin.pavlik100@gmail.com>
2020-09-30 01:24:41 +0200

    Clean up

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-// Jak napsat Visitor v JS -> https://medium.com/dailyjs/compiler-in-javascript-using-antlr-9ec53fd2780f
-
-// Ohledne toho mismatched input erroru.. https://stackoverflow.com/questions/29777778/antlr-4-5-mismatched-input-x-expecting-x
 const antlr4 = require('antlr4');
 const QueryLangLexer = require('./QueryLangLexer').QueryLangLexer;
 const QueryLangParser = require('./QueryLangParser').QueryLangParser;
@@ -18,67 +15,40 @@ PrintQueryListener.prototype.constructor = PrintQueryListener;
 
 // override default listener behavior
 PrintQueryListener.prototype.enterNameQuery = function (ctx) {
-  console.log("name query:", ctx.getText(), 'tokens:', ctx.getTokens());
+  console.log("name query:", ctx.getText());
 };
 PrintQueryListener.prototype.enterCmcQuery = function (ctx) {
-  console.log("cmc query:", ctx.getText(), 'tokens:', ctx.getTokens());
+  console.log("cmc query:", ctx.getText());
 };
 PrintQueryListener.prototype.enterCommanderIdentityQuery = function (ctx) {
-  console.log("cmd identity query:", ctx.getText(), 'tokens:', ctx.getTokens());
+  console.log("cmd identity query:", ctx.getText());
 };
 PrintQueryListener.prototype.enterTextQuery = function (ctx) {
-  console.log("text query:", ctx.getText(), 'tokens:', ctx.getTokens());
+  console.log("text query:", ctx.getText());
 };
 PrintQueryListener.prototype.enterNumberValue = (ctx) => {
-  console.log("Enter: number:", ctx.getText())
+  console.log("value: number:", ctx.getText())
 }
 PrintQueryListener.prototype.enterValue = (ctx) => {
-  console.log("Enter: value:", ctx.getText())
-}
-PrintQueryListener.prototype.enterOperatorEq = (ctx) => {
-  console.log("Enter: operator:", ctx.getText())
+  console.log("value: any:", ctx.getText())
 }
 
 
 const input = 'cmc < 3 draw discard exile';
 // const input = "cmc=2 NAME:\"Martin picus kreten\" N:\"Izzetx\""
 const chars = new antlr4.InputStream(input);
-console.log('chars:', chars);
+// console.log('chars:', chars);
 const lexer = new QueryLangLexer(chars);
-console.log('lexer:', lexer);
+// console.log('lexer:', lexer);
 const tokens = new antlr4.CommonTokenStream(lexer);
-console.log('tokens:', tokens);
+// console.log('tokens:', tokens);
 const parser = new QueryLangParser(tokens);
-console.log('parser:', parser);
+// console.log('parser:', parser);
 parser.buildParseTrees = true;
 const tree = parser.wholeQuery();
 
-console.log('---------::');
+console.log('---------');
 
-/*
-class Visitor {
-  visitChildren(ctx) {
-    if (!ctx) {
-      return;
-    }
-
-    console.log('ctx::', ctx.toStringTree())
-
-    if (ctx.children) {
-      return ctx.children.map(child => {
-        if (child.children && child.children.length != 0) {
-          return child.accept(this);
-        } else {
-          return child.getText();
-        }
-      });
-    }
-  }
-}
-
-console.log(tree.accept(new Visitor()));
-
-*/
 
 var printer = new PrintQueryListener();
 antlr4.tree.ParseTreeWalker.DEFAULT.walk(printer, tree);
